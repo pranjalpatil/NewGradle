5668c1cc624e919926b395ce47d448a537c85121
Pranjal Patil <pranjalpatil30@gmail.com>
2017-05-11 12:00:43 +0530

    Update AccountTest.java

--- BankApplicationUsingGradle/src/test/java/com/capgemini/test/AccountTest.java
+++ BankApplicationUsingGradle/src/test/java/com/capgemini/test/AccountTest.java
@@ -5,55 +5,111 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import com.capgemini.exceptions.InsufficientInitialBalanceException;
+import com.capgemini.exceptions.InsufficientBalanceException;
+import com.capgemini.exceptions.InsufficientInitialAmountException;
+import com.capgemini.exceptions.InvalidAccountNumberException;
 import com.capgemini.model.Account;
 import com.capgemini.repository.AccountRepository;
 import com.capgemini.service.AccountService;
 import com.capgemini.service.AccountServiceImpl;
-import static org.mockito.Mockito.when;
-import static org.junit.Assert.assertEquals;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 public class AccountTest {
+
+	AccountService accountService;
 	
 	@Mock
 	AccountRepository accountRepository;
-	
-	AccountService accountService;
-
 	@Before
 	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);//dummy object accountRepository object is created
+		MockitoAnnotations.initMocks(this);
+		
 		accountService = new AccountServiceImpl(accountRepository);
 	}
+
+	/*
+	 * create account
+	 * 1.when the amount is less than 500 then system should throw exception
+	 * 2.when the valid info is passed account should be created successfully
+	 */
 	
-	// use story for createAccount
-	//1 when the amount is less than 500 then it should throw exception
-	//2 when the valid info is passed account object should be created successfully
-	
-	@Test(expected = com.capgemini.exceptions.InsufficientInitialBalanceException.class)
-	public void whenTheAmountIsLessThan500ItShouldThrowException() throws InsufficientInitialBalanceException
+	//******** Create Account*********
+	@Test(expected=com.capgemini.exceptions.InsufficientInitialAmountException.class)
+	public void whenTheAmountIsLessThan500SystemShouldThrowException() throws InsufficientInitialAmountException
 	{
-		accountService.createAccount(101,200);
-		
+		accountService.createAccount(101, 400);
 	}
 	
 	@Test
-	public void whenTheValidInfoIsPassedAccountObjectShouldBeCreatedSuccessfully() throws InsufficientInitialBalanceException
+	public void whenTheValidInfoIsPassedAccountShouldBeCreatedSuccessfully() throws InsufficientInitialAmountException
 	{
-		Account account = new Account();
-		
+		Account account =new Account();
 		account.setAccountNumber(101);
 		account.setAmount(5000);
+		when(accountRepository.save(account)).thenReturn(true);
+		assertEquals(account, accountService.createAccount(101, 5000));
+	}
+
+	//******** Deposit Amount*********
+	@Test
+	public void whenTheAmountIsDepositedIntoValidAccountTxnShouldBeSuccessful() throws InvalidAccountNumberException
+	{
+		Account account = new Account();
+		account.setAccountNumber(101);
+		account.setAmount(800);
+		when(accountRepository.searchAccount(101)).thenReturn(account);
 		
-		when(accountRepository.createAccount(account)).thenReturn(true);
 		
-		assertEquals(account.getAmount(),accountService.createAccount(101, 5000).getAmount());
+		assertEquals(1300, accountService.depositAmount(101, 500));
+		
+		
+	}
+	@Test(expected=com.capgemini.exceptions.InvalidAccountNumberException.class)
+	public void whenTheInvalidAccountSystemShouldGiveException() throws InvalidAccountNumberException
+	{
+		Account account = new Account();
+		account.setAccountNumber(101);
+		account.setAmount(800);
+		when(accountRepository.searchAccount(101)).thenReturn(account);
+		
+		
+		accountService.depositAmount(102, 500);
+		
+		
 	}
 	
-// when the valid info info is passed , amount should be deposited
+	//******** Withdraw Amount*********
+	@Test
+	public void whenTheAmountIsWithdrawnFromAccountTxnShouldBeSuccessful() throws InvalidAccountNumberException, InsufficientBalanceException
+	{
+		Account account = new Account();
+		account.setAccountNumber(101);
+		account.setAmount(8000);
+		when(accountRepository.searchAccount(101)).thenReturn(account);
+		
+		assertEquals(7000, accountService.WithdrawAmount(101, 1000));
+	}
 	
-	//when the balance is insufficient it should throw exception
-
+	@Test(expected=com.capgemini.exceptions.InvalidAccountNumberException.class)
+	public void whenTheInvalidAccountWithdrawlSystemShouldGiveException() throws InvalidAccountNumberException, InsufficientBalanceException
+	{
+		Account account = new Account();
+		account.setAccountNumber(101);
+		account.setAmount(8000);
+		when(accountRepository.searchAccount(101)).thenReturn(account);
+		accountService.WithdrawAmount(102, 500);	
+	}
 	
-
+	@Test(expected=com.capgemini.exceptions.InsufficientBalanceException.class)
+	public void whenTheInsufficientAmountWithdrawlSystemShouldGiveException() throws InvalidAccountNumberException, InsufficientBalanceException
+	{
+		Account account = new Account();
+		account.setAccountNumber(101);
+		account.setAmount(8000);
+		when(accountRepository.searchAccount(101)).thenReturn(account);
+		
+		
+		accountService.WithdrawAmount(101, 50000);	
+	}
 }
